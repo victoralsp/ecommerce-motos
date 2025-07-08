8e44feb60ff2b1152e496d97c86218938013a84d
Victor Hugo Alves Pinheiro <victorpinheiro1008@gmail.com>
2025-07-08 17:39:12 -0300

    Moto dr160s adicionada, e ajuste no header, mais escuro

--- src/components/MainHeroCarrossel/MainHeroCarrossel.jsx
+++ src/components/MainHeroCarrossel/MainHeroCarrossel.jsx
@@ -3,7 +3,7 @@ import styles from './MainHeroCarrossel.module.scss';
 import imgFazer150 from '../../assets/imagens/fazer150.png';
 import imgCg160 from '../../assets/imagens/cg160.png';
 import imgNs160 from '../../assets/imagens/ns160.png';
-// import imgDr160s from '../../assets/imagens/dr160.png';
+import imgDr160s from '../../assets/imagens/dr160s.png';
 import PosicaoItensCarrossel from "../PosicaoItensCarrossel/PosicaoItensCarrossel";
 
 export default function MainHeroCarrossel() {
@@ -11,25 +11,32 @@ export default function MainHeroCarrossel() {
     const infosMotosCarrossel = [
         {
             url: imgFazer150,
-            widthImg: '720px', 
-            nomeMoto: 'Fazer 150', 
+            widthImg: '720px',
+            nomeMoto: 'Fazer 150',
             corTexto: '#727272',
             bgColor: 'radial-gradient(circle, rgba(38, 38, 38, 1) 0%,rgb(0, 0, 0) 100%)'
         },
         {
-            url: imgNs160, 
-            widthImg: '690px', 
-            nomeMoto: 'BAJAJ NS 160', 
+            url: imgNs160,
+            widthImg: '690px',
+            nomeMoto: 'BAJAJ NS 160',
             corTexto: '#b5292c',
-            bgColor: 'radial-gradient(circle,rgba(181, 41, 44, 1) 0%, rgb(0, 0, 0) 100%)'
+            bgColor: 'radial-gradient(circle, rgba(181, 41, 44, 1) 0%, rgb(0, 0, 0) 100%)'
         },
         {
-            url: imgCg160, 
-            nomeMoto: 'CG 160 TITAN', 
-            widthImg: '900px', 
+            url: imgCg160,
+            nomeMoto: 'CG 160 TITAN',
+            widthImg: '900px',
             corTexto: '#f97109',
-            bgColor: 'radial-gradient(circle,rgba(249, 113, 9, 1) 0%, rgb(0, 0, 0) 100%)'
-        }
+            bgColor: 'radial-gradient(circle, rgba(249, 113, 9, 1) 0%, rgb(0, 0, 0) 100%)'
+        },
+        {
+            url: imgDr160s,
+            nomeMoto: 'Dr 160s haojue',
+            widthImg: '750px',
+            corTexto: '#d4d4d4',
+            bgColor: 'radial-gradient(circle, rgba(218, 218, 217, 1) 0%, rgba(0, 0, 0, 1) 100%)'
+        },
     ]
 
     const [indexAtual, setIndexAtual] = useState(0)
@@ -44,7 +51,7 @@ export default function MainHeroCarrossel() {
         setTimeout(() => {
             setIndexAtual(novoIndex);
             setAnimacao("entrada");
-        }, 300);
+        }, 500);
     };
 
 
@@ -69,19 +76,23 @@ export default function MainHeroCarrossel() {
 
     return (
         <article className={styles.articleMainHeroCarrossel} style={{ background: infosMotosCarrossel[indexAtual].bgColor }}>
-            <p style={{ color: infosMotosCarrossel[indexAtual].corTexto }}>{infosMotosCarrossel[indexAtual].nomeMoto}</p>
-            <img 
-                src={infosMotosCarrossel[indexAtual].url}  
-                style={{ width: infosMotosCarrossel[indexAtual].widthImg }}  
+            <p
+                style={{ color: infosMotosCarrossel[indexAtual].corTexto }}
+                className={`${styles.textoCarrossel} ${styles[`animacaoTexto-${animacao}-${direcao}`]}`}>
+                {infosMotosCarrossel[indexAtual].nomeMoto}
+            </p>
+            <img
+                src={infosMotosCarrossel[indexAtual].url}
+                style={{ width: infosMotosCarrossel[indexAtual].widthImg }}
                 className={`${styles.imagemMotoCarrossel} ${styles[`${animacao}-${direcao}`]}`}
                 alt="foto da moto"
             />
             <PosicaoItensCarrossel
-                passarSlide = {nextSlide}
-                voltarSlide = {prevSlide}
-                infosMotosCarrossel = {infosMotosCarrossel}
-                mudarPeloDot = {mudarPeloDot}
-                indexAtual = {indexAtual}
+                passarSlide={nextSlide}
+                voltarSlide={prevSlide}
+                infosMotosCarrossel={infosMotosCarrossel}
+                mudarPeloDot={mudarPeloDot}
+                indexAtual={indexAtual}
             />
         </article>
     )
