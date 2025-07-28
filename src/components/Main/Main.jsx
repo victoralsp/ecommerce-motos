9deab9389e781cee7b1eae2a79622ac38b095549
Victor Hugo Alves Pinheiro <victorpinheiro1008@gmail.com>
2025-07-28 19:12:52 -0300

    imagens das logos adds e marquee feito para 1080p

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,11 +3,23 @@ import MainHeroCarrossel from "../MainHeroCarrossel/MainHeroCarrossel";
 import LogosPassando from "../LogosPassando/LogosPassando";
 import styles from './Main.module.scss';
 
+import LogoBajaj from '../../assets/imagens/bajaj-logo.png';
+import LogoHaojue from '../../assets/imagens/Logo-Haojue.png';
+import LogoHonda from '../../assets/imagens/honda_logo.png';
+import LogoYamaha from '../../assets/imagens/yamaha-logo.png';
+
+const LogosMarcas = [
+    LogoBajaj,
+    LogoHaojue,
+    LogoHonda,
+    LogoYamaha
+]
+
 export default function Main() {
     return (
         <main className={styles.main}>
             <MainHeroCarrossel />
-            <LogosPassando/>
+            <LogosPassando LogosMarcas = {LogosMarcas} />
         </main>
     )
 }
