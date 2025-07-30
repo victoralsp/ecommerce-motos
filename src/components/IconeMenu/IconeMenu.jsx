bd095027fb031a8cf561a5ee85cbe5bb76412b41
Victor Hugo Alves Pinheiro <victorpinheiro1008@gmail.com>
2025-07-30 15:27:55 -0300

    adicionando aria-label nos botões para melhorar acessibilidade

--- src/components/IconeMenu/IconeMenu.jsx
+++ src/components/IconeMenu/IconeMenu.jsx
@@ -6,7 +6,11 @@ import styles from './IconeMenu.module.scss';
 export default function IconeMenu({ onToggleMenu, isOpen }) {
 
     return (
-        <button className={styles.IconeMenu} onClick={onToggleMenu}>{isOpen ?  <CgClose/> : <TbMenuDeep/>
+        <button 
+            className={styles.IconeMenu} 
+            onClick={onToggleMenu}
+            aria-label={isOpen ? 'Fechar menu' : 'Abrir menu'}
+            >{isOpen ?  <CgClose/> : <TbMenuDeep/>
 }</button>
     )
 }
