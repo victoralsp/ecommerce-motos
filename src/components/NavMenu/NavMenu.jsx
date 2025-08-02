4944ebc2fcfa2e78b2919f32cbb69cfe99ffe0a0
Victor Hugo Alves Pinheiro <131179996+victoralsp@users.noreply.github.com>
2025-08-02 20:46:48 -0300

    ajuste de responsividade: dots maiores no mobile, e melhor posicionados para tablets. Efeitos: hover no header

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -5,7 +5,7 @@ export default function NavMenu({ isOpen }) {
     return (
         <nav className={`${styles.NavBar} ${isOpen ? styles.open : styles.NavBar}`} > 
             <ul>
-                <li><a href="/">Home</a></li>
+                <li><a href="/">Ofertas</a></li>
                 <li><a href="/">Test Ride</a></li>
                 <li><a href="/">Lojas</a></li>
                 <li><a href="/">Contatos</a></li>
